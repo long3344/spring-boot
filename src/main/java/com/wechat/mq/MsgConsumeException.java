f933bac347ef7e52b28444523048aeb5b7d0d8e8
tianwenlong <tianwenlong51jk.com>
2017-05-22 09:50:50 +0800

    加入bootstrap,swiper,实现redis缓存

---
+++ src/main/java/com/wechat/mq/MsgConsumeException.java
@@ -0,0 +1,38 @@
+package com.wechat.mq;
+
+import com.wechat.exception.BusinessLogicException;
+
+/**
+ * 描述: 消息消费异常
+ * 作者: wangzhengfei
+ * 创建日期: 2017-02-13
+ * 修改记录:
+ */
+public class MsgConsumeException extends BusinessLogicException {
+
+    static final long serialVersionUID = -1256516991663579371L;
+
+    public MsgConsumeException() {
+        super();
+    }
+
+    public MsgConsumeException(String message) {
+        super(message);
+    }
+
+    public MsgConsumeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MsgConsumeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MsgConsumeException(String message, Throwable cause,
+                                     boolean enableSuppression,
+                                     boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+
+}
